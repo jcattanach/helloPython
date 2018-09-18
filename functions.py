7ee334be433a143be961c1c04cd317ac31da4b43
Jack Cattanach <jackcattanach@live.com>
2018-09-18 11:44:39 -0500

    this is a meaasage about a commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+# def greet(name):
+#     print("hello, ", name)
+#
+# input_name = input("Enter your name: ")
+# greet(input_name)
+#
+# # or
+#
+# def greet(name):
+#     print("hello, {0}".format(name))
+#
+# input_name = input("Enter your name: ")
+# greet(input_name)
+
+# def display_car_info(make, model):
+#     print("------------------------")
+#     print("Make {0} - Model {1}".format(make,model))
+
+#///////////////////////////////////////////////////
+#conditions
